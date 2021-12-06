eadafb8f87f03bd70e7e3a5ec7d83c88e43cf3de
wellpinho <wellpinho@outlook.com>
2021-12-06 20:38:17 -0300

    🆕 NEW: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Routes } from './routes'
+import { GlobalStyle } from './styles/Global';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 }
 
