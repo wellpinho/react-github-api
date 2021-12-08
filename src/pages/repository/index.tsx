7be030ef71ee4e445a2a6892e35ff5f7818d75cf
wellpinho <wellpinho@outlook.com>
2021-12-08 20:10:13 -0300

    🆕 NEW: ul and li

--- src/pages/repository/index.tsx
+++ src/pages/repository/index.tsx
@@ -20,6 +20,30 @@ export const Repository: React.FC = () => {
         </Link>
         Repositorios {params.repository}
       </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="Wellington Pinho" />
+          <div>
+            <strong>wellpinho/love-rev2</strong>
+            <p>Repositório do curso gratuito de react js</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>2334</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>210</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>79</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
     </>
   );
 }
@@ -42,6 +66,59 @@ const Header = styled.header`
 
     svg {
       margin-left: 4px;
+    }
+  }
+`
+
+const RepositoryInfo = styled.section`
+  margin-top: 80px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      & + li {
+        margin-left: 80px;
+      }
+
+      strong {
+        display: block;
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #737380;
+      }
     }
   }
 `
