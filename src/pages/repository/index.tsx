334a5881114fe219d43c9fd34243b7a8e1ce5813
wellpinho <wellpinho@outlook.com>
2021-12-08 19:59:13 -0300

    🆕 NEW: link to home

--- src/pages/repository/index.tsx
+++ src/pages/repository/index.tsx
@@ -1,4 +1,7 @@
-import { useRouteMatch } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
+import styled from "styled-components";
+import { FiChevronsLeft } from 'react-icons/fi'
+import Logo from './../../assets/logo.svg'
 
 interface IParams {
   repository: string
@@ -8,7 +11,37 @@ export const Repository: React.FC = () => {
   const { params } = useRouteMatch<IParams>()
 
   return (
-    <h1>Repositorios {params.repository}</h1>
+    <>
+      <Header>
+        <img src={Logo} alt="" />
+        <Link to="/">
+          <FiChevronsLeft />
+          voltar
+        </Link>
+        Repositorios {params.repository}
+      </Header>
+    </>
   );
 }
 
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color .2s;
+
+    &:hover {
+      color: #666666;
+    }
+
+    svg {
+      margin-left: 4px;
+    }
+  }
+`
